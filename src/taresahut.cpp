1f13762eca033af7f8ddf6ca5fa3698c7c95d568
junjoseaguilar <juanjoseaguilarhn@gmail.com>
2020-07-05 21:30:54 -0600

    commit inicial

---
+++ src/taresahut.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int contador=0;
+    while (contador<10)
+    {
+        cout<< "numero:"<< contador<<endl;
+        contador++;
+    }
+    cout<<"fin";
+    return 0;
+}
